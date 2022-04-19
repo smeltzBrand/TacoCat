13243a7337af21dced7eeeb3825ba6747d4e8daf
smeltzBrand <101213259+smeltzBrand@users.noreply.github.com>
2022-04-19 10:43:08 -0400

    first commit

---
+++ js/site.js
@@ -0,0 +1,69 @@
+//Step ONE - controller accept requests
+function getMessage() {
+
+    let msg = document.getElementById("txtMessage").value;
+
+    //check if the uesr entered a string
+    if (msg == "") {
+        Swal.fire("Oops!","Please enter text.");
+    } else {
+
+        msg = msg.toUpperCase();
+
+        const regex = /[^a-z0-9]/gi;
+        msg = msg.replace(regex, "");
+
+        let palinObj = reverseMessageB(msg);
+        displayMessage(palinObj);
+    }
+
+}
+
+function reverseMessageA(msg) {
+    let revMessage = "";
+
+    //decrimenting loop
+    for (let i = msg.length - 1; i >= 0; i--) {
+
+        let char = msg[i];
+        revMessage += char;
+
+    }
+
+    return revMessage;
+}
+
+function reverseMessageB(originalWord) {
+
+    let revWord = "";
+
+    let returnObj = {};
+
+    revWord = originalWord.split("").reverse().join("");
+
+    if (revWord == originalWord) {
+        returnObj.message = `The word/phrase is a palindrome!`;
+        returnObj.IsPalindrome = true;
+    } else {
+        returnObj.message = `The word/phrase is not a palindrome :(`;
+        returnObj.IsPalindrome = false;
+    }
+
+    returnObj.originalWord = originalWord;
+    returnObj.reversedWord = revWord;
+
+    // IsPalindrome
+    // originalWord
+    // the reversed word
+    // the message
+
+    return returnObj;
+}
+
+//Final Step - view
+function displayMessage(palinObj) {
+
+    //first get the ol element from the page
+    element = document.getElementById("results");
+    element.innerHTML = `${palinObj.message} - ${palinObj.originalWord}`;
+}
